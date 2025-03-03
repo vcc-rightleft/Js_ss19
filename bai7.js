a13df384f41eb368078aa61525cbce0e608dfce1
vcc-rightleft <trumfaifai00ngo@gmail.com>
2025-03-03 10:41:44 +0700

    Update bai7.js

--- bai7.js
+++ bai7.js
@@ -1,5 +1,5 @@
 let a = Number(prompt("Nhap so a: "));
 let b = Number(prompt("Nhap so b: "));
 let c = Number(prompt("Nhap so c: "));
-let max = Math.max(a, b, c);
+let max = a > b ? (a > c ? a : c) : b > c ? b : c;
 alert("So lon nhat trong 3 so la:" + max);
